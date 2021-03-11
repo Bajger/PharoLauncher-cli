b179c5ebd0a1cba67d9a903a93231d62bfdb9604
David Bajger <bajger@gmail.com>
2021-03-11 18:21:22 +0100

    Added windows batch file for shortcut purposes.

---
+++ script/pharoLauncher.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Windows batch file to shortcut call of vm executable with image argument with clap parameter
+..\..\vms\80-x64\PharoConsole.exe --headless P8-64b-PharoLauncher-cli.image clap %*
