7bda0505db6fa4b57a927f7a25d27334d6a144ca
DavidBajger <bajger@gmail.com>
2021-05-20 13:43:38 +0200

    added script for mac

--- script/pharoLauncher.bat
+++ script/pharoLauncher.bat
@@ -1,3 +1,3 @@
 @echo off
 REM Windows batch file to shortcut call of vm executable with image argument with clap parameter
-..\..\vms\80-x64\PharoConsole.exe --headless P8-64b-PharoLauncher-cli.image clap %*
+..\..\vms\80-x64\PharoConsole.exe --headless *.image clap %*
